956ac35dc38e8202dfd3a0e824166615a0ecb810
Zaria Hallager <120356303+ZariaHallager@users.noreply.github.com>
2026-04-06 00:06:23 -0400

    3-30 push

---
+++ trees/SameBinaryTree.py
@@ -0,0 +1,63 @@
+# Same Binary Tree
+# Easy
+# Topics
+# Company Tags
+# Hints
+# Given the roots of two binary trees p and q, return true if the trees are equivalent, otherwise return false.
+
+# Two binary trees are considered equivalent if they share the exact same structure and the nodes have the same values.
+
+# Example 1:
+
+
+
+# Input: p = [1,2,3], q = [1,2,3]
+
+# Output: true
+# Example 2:
+
+
+
+# Input: p = [4,7], q = [4,null,7]
+
+# Output: false
+# Example 3:
+
+
+
+# Input: p = [1,2,3], q = [1,3,2]
+
+# Output: false
+# Constraints:
+
+# 0 <= The number of nodes in both trees <= 100.
+# -100 <= Node.val <= 100
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        q1 = deque([p])
+        q2 = deque([q])
+
+        while q1 and q2:
+            for _ in range(len(q1)):
+                nodeP = q1.popleft()
+                nodeQ = q2.popleft()
+
+                if nodeP is None and nodeQ is None:
+                    continue
+                if nodeP is None or nodeQ is None or nodeP.val != nodeQ.val:
+                    return False
+
+                q1.append(nodeP.left)
+                q1.append(nodeP.right)
+                q2.append(nodeQ.left)
+                q2.append(nodeQ.right)
+
+        return True
